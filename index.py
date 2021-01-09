a46418cf89f4b012e9dab06b106990cc03eaebd4
fatiiates <fatiiates@gmail.com>
2021-01-09 14:46:23 +0300

    0.1.0

--- index.py
+++ index.py
@@ -2,16 +2,18 @@ import numpy as np
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt
+from sklearn import datasets, svm
 from sklearn.model_selection import train_test_split, learning_curve, cross_val_score, StratifiedKFold
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, plot_roc_curve, auc
-from termcolor import colored 
+from sklearn.metrics import confusion_matrix, plot_roc_curve, auc, roc_curve
+from itertools import cycle
+from scipy import interp
 
 
 def importData():
     dataset = pd.read_csv("data.csv")
 
-    dataset = dataset.drop(["id", "Unnamed: 32"], axis=1)
+    dataset = dataset.drop(["id"], axis=1)
 
     M = dataset[dataset.diagnosis == "M"]
 
@@ -67,64 +69,66 @@ def f_measure(tp, fp, fn):
 
 
 def mean_std(estimator, X, y):
-   
+
     df = cross_val_score(estimator, X, y,
                          cv=10, scoring="accuracy")
-    print("Accuracy(mean): {0}\n".format(df.mean()))
+    print("Accuracy(mean): {0}".format(df.mean()))
     print("Standart sapma: {0}\n".format(df.std()))
 
     return df.mean(), df.std()
 
 
-def drawROC(estimator, X, y):
-    X = np.c_[X[0], X[1]]
-    y = np.c_[y[0],]
-    print(y.shape)
-    tprs = []
-    aucs = []
-    mean_fpr = np.linspace(0, 1, 100)
+def drawROC():
+    bc = datasets.load_breast_cancer()
+    X = bc.data
+    y = bc.target
 
-    classifier = GaussianNB()
-    cv = StratifiedKFold(n_splits=6)
+    X, y = X[y != 2], y[y != 2]
     n_samples, n_features = X.shape
+
+    # Add noisy features
     random_state = np.random.RandomState(0)
-    fig, ax = plt.subplots()
-    for i, (train, test) in enumerate(cv.split(X, y)):
-        classifier.fit(X[train], y[train])
-        viz = plot_roc_curve(classifier, X[test], y[test],
-                            name='ROC fold {}'.format(i),
-                            alpha=0.3, lw=1, ax=ax)
-        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-        interp_tpr[0] = 0.0
-        tprs.append(interp_tpr)
-        aucs.append(viz.roc_auc)
+    X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
+    cv = StratifiedKFold(n_splits=10)
+    classifier = GaussianNB()
 
-    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-            label='Chance', alpha=.8)
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    aucs = []
+    colors = cycle(['cyan', 'indigo', 'seagreen', 'red', 'blue', 'darkorange'])
+    lw = 2
 
-    mean_tpr = np.mean(tprs, axis=0)
+    i = 0
+    for (train, test), color in zip(cv.split(X, y), colors):
+        probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        mean_tpr += np.interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        plt.plot(fpr, tpr, lw=.5, color=color, linestyle="--",
+                 label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+        i += 1
+
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=1, color='k',
+             label='Luck')
+
+    mean_tpr /= cv.get_n_splits(X, y)
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(mean_fpr, mean_tpr, color='b',
-            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-            lw=2, alpha=.8)
+    print("AUC (mean): {0}".format(mean_auc))
+    print("Standart sapma: {0}\n".format(np.std(aucs)))
+    plt.plot(mean_fpr, mean_tpr, color='g',
+             label='Mean ROC (AUC = %0.2f)' % mean_auc, lw=lw)
 
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                    label=r'$\pm$ 1 std. dev.')
-
-    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-        title="Receiver operating characteristic example")
-    ax.legend(loc="lower right")
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
     plt.show()
-
-    """datas = plot_roc_curve(estimator, X, y)
-    print("AUC(means): {0}\n".format(datas.roc_auc))
-    print("AUC(means): {0}\n".format(datas.roc_auc))
-    plt.show()"""
 
 
 def learningCurve(estimator=None, title="GaussianNB", X=None, y=None, cv=None,
@@ -210,7 +214,7 @@ def main():
         elif expected == 7:
             learningCurve(estimator=nb, X=data_train, y=result_train)
         elif expected == 8:
-            drawROC(nb, [data_train, result_train], [data_test, result_test])
+            drawROC()
         elif expected == 9:
             confusionMatrix(tn, fp, fn, tp)
         elif expected == 10:
